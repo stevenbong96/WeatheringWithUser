b6a6ef23b9b915f9629a982a54ffc7c7bd89c43c
Steven Bong <stevenseal99@yahoo.com>
2020-09-21 17:40:22 -0700

    Had tutor with Andres to help with connecting the add button to make the list and weather AJAX. Also, Worked on the AJAX forecast and make the 5 days forecast. Modified the HTML content to not use the append to avoid the multiple results

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 // Declare global variables
 // var cities = ["Seattle", "San Francsico", "Portland", "Los Angeles"];
-var cityName = "San Francisco";
-var addCity = $("#addcity");
+var cityName = [];
+// var addCity = $("#addcity");
+let cityInput = $("#citytext");
 
 // Declare the API Key
 var APIKey = "9225ddf71ee9bfd143d39d7740347c51";
 
-// Declare the queryURL
-var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=" + APIKey;
+// showListOfCity(cityName);
 
 // Create list of city
 function showListOfCity(createList){
     $("#listcity").empty();
 
-    var list = $("#textarea");
-
     // Create for loop
-    for(i = 0; i < list.length;i++){
-        var cityList = $("<button>");
-        cityList.addClass();
+    for(i = 0; i < createList.length;i++){
+        var cityButton = $("<button>");
+        cityButton.text(createList[i]);
+        $("#listcity").append(cityButton);
+        
     }
 }
 
 // Create function to call the ajax function to run the API
-function displayWeather(){
-    // Grab the value from the textarea
-    // var inputCity = $("#citytext").val().trim();
-
+function displayWeather(cities){
     // Declare the weather queryURL
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cities + "&appid=" + APIKey;
+
+    // Declare the queryURL
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cities + "&appid=" + APIKey;
 
     // Call the ajax 
     $.ajax({
@@ -38,40 +38,35 @@ function displayWeather(){
         console.log(response);
         
         // Create the city name display
-        var displayCity = $("<h2>");
-        displayCity.text(cityName);
-        $("#cityname").append(displayCity);
+        var displayCity = $("#cityname");
+        displayCity.text(cities);
 
         // Display current time
-        var currentDate = $("<h4>");
+        var currentDate = $("#currenttime");
         currentDate.text(moment().format('L'));
-        $("#currenttime").append(currentDate);
 
         // Display the weather icon
-        var displayIcon = $("#weathericon");
-        displayIcon.text(response.weather);
+        // var displayIcon = $("#weathericon");
+        // displayIcon.text(response.weather);
 
         // Create temperature display
-        var displayTemp = $("<h4>");
+        var displayTemp = $("#citytemp");
         var tempFahr = ((response.main.temp) - 273.15) * 1.80 + 32;
         displayTemp.text("Temperature: " + tempFahr.toFixed(0) + " F");
-        $("#citytemp").append(displayTemp);
 
         // Create humidity display
-        var displayHumidity = $("<h4>");
-        displayHumidity.text("Humidity: " + response.main.humidity);
-        $("#cityhumidity").append(displayHumidity);
+        var displayHumidity = $("#cityhumidity");
+        displayHumidity.text("Humidity: " + response.main.humidity + " %");
 
         // Create wind speed display
-        var displayWind = $("<h4>");
-        displayWind.text("Wind Speed: " + response.wind.speed);
-        $("#citywind").append(displayWind);
+        var displayWind = $("#citywind");
+        displayWind.text("Wind Speed: " + response.wind.speed + " MPH");
 
         // Create UV Index display
-        var displayUV = $("<h4>");
-        displayUV.text();
-        displayUV.addClass();
-        $("#cityuv").append(displayUV);
+        // var displayUV = $("<h4>");
+        // displayUV.text();
+        // displayUV.addClass();
+        // $("#cityuv").append(displayUV);
     })
 
     // Call the AJAX for forecast
@@ -79,21 +74,47 @@ function displayWeather(){
         url: forecastURL,
         method: "GET"
     }).then(function(responseForecast){
-        console.log(forecastURL);
+        console.log(responseForecast);
+        // Create a for loop
+        for(j = 0; j < responseForecast.list.length; j+=8){
+
+            // Display city name
+            var forecastText = $("#name-" + j);
+            forecastText.text(responseForecast.city.name);
+
+            // Display city temperature
+            var forecastTemp = $("#temp-" + j);
+            var forecastFahr = ((responseForecast.list[j].main.temp) - 273.15) * 1.80 + 32;
+            forecastTemp.text("Temperature: " + forecastFahr.toFixed(0) + " F");
+
+            // Display Forecast Humidity
+            var forecastHumid = $("#humid-" + j);
+            forecastHumid.text("Humidity: " + responseForecast.list[j].main.humidity + " %");
+
+        }
     })
+
+    // Create AJAX for UV Index
+    // $.ajax({
+    //     url: uvURL,
+    //     method: "GET"
+    // }).then(function(responseUV){
+    //     console.log(responseUV);
+    // })
+
 }
 
 // Create function to display the weather data for each city
-function reloadDisplayCity(){
-    // // Looping through an array of cities
-    // for(i = 0; i < cityName.length; i++){
-    //     var reDisplay = $("<button>");
-    //     reDisplay.addClass();
-    //     reDisplay.attr("",cities[i]);
-    //     reDisplay.text(cities[i]);
-    //     $("#citylist").append(reDisplay);
-    // }
-}
+// function reloadDisplayCity(){
+//     // Looping through an array of cities
+//     for(i = 0; i < cityName.length; i++){
+//         var reDisplay = $("<button>");
+//         reDisplay.addClass();
+//         reDisplay.attr("",cities[i]);
+//         reDisplay.text(cities[i]);
+//         $("#citylist").append(reDisplay);
+//     }
+// }
 
 // local storage function
 function saveToLocal(key, value){
@@ -101,28 +122,38 @@ function saveToLocal(key, value){
 }
 
 // Create event listener for user to search their city
-addCity.on("click", "button", function(event){
+$(document).on("click", "button", function(event){
     // To prevent the page refresh itself
     event.preventDefault();
-
+    
     // Grab the input from the text box
-    var inputCity = $("#textarea").val().trim();
+    var inputCity = cityInput.val().trim();
+    console.log(inputCity);
 
     // Create if/else conditional statement 
-    if(inputCity === " "){
-        return
-    }
+    // if(inputCity === " "){
+    //     return
+    // }
 
     // Adding movies to the list
-    cityName.push(listMovies);
+    for(z = 0; z < cityName.length; z++){
+        if(cityName[z] === inputCity){
+            displayWeather(inputCity);
+            return;
+        } 
+    }
+
+    cityName.push(inputCity);
+    displayWeather(inputCity);
+    showListOfCity(cityName);
 
     // Save to local storage
-    saveToLocal(this.id,$(this).val());
+    // saveToLocal(this.id,$(this).val());
 
     // Call the 
-    reloadDisplayCity();
+    // reloadDisplayCity();
 })
 
 // Call the 
-reloadDisplayCity();
-displayWeather();
+// reloadDisplayCity();
+// displayWeather();
